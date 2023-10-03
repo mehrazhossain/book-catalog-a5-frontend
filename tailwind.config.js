3300ae08ab061a8885e8472fac90f96d52a4452c
Mehraz Hossain <mehrazhossain54@gmail.com>
2023-10-03 11:03:13 +0600

    Configure daisy theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   daisyui: {
-    themes: ["cupcake"],
+    themes: ["forest"],
   },
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
-
+};
