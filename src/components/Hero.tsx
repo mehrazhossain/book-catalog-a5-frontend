ac935196f865d94cf6d7db9d9f75520d5a145f50
Mehraz Hossain <mehrazhossain54@gmail.com>
2023-10-03 10:37:52 +0600

    Added home page hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div className="">
+      <div className="text-center">
+        <h1 className="text-3xl m-10">Recently Added Books</h1>
+      </div>
+    </div>
+  );
+}
